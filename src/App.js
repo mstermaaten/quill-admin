35f9435aa2716390c14f5c31c537bf98216ace5d
Marisa Angel <HouseofBo@imac-van-termaaten-familie.local>
2019-08-06 14:06:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactQuill, { Quill }  from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.core.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    }
+  }
+ 
+  modules = {
+    toolbar: [
+      ['formula'],
+      [{ 'header': [1, 2, false] }],
+      [{'align': ['','center', 'right', 'justify']}],
+      ['blockquote', 'code-block'],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image', 'video']
+    ],
+  }
+ 
+  formats = [
+    'formula',
+    'header',
+    'align',
+    'blockquote', 'code-block',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video'
+  ]
+
+
+ 
+  render() {
+    return (
+      <div className="container">
+        <div className="text-editor">
+        <ReactQuill 
+                    className="editor"
+                    theme="snow"
+                    modules={this.modules}
+                    formats={this.formats}>
+        </ReactQuill>
+      </div>
+        <style jsx>{`
+          .container {
+            width: 100%;
+            padding: 20px;
+          }
+
+          .text-editor {
+            width: 40%;
+            display: block;
+            margin: auto;
+          }
+
+          .editor {
+            min-height: 500px;
+          }
+        `}</style>
+      </div>
+    );
+  }
 }
 
 export default App;
